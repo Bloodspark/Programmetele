815b1d40f46b9e9acfe556507989e26cf2af40cc
sylvaincaballero <sylvaincaballero@hotmail.fr>
2015-05-12 11:22:50 +0200

    Strcture de base du projet. Créations des classes héritées de Emission.

---
+++ ProgrammeTele/src/main/java/fr/manastria/programmetele/Application.java
@@ -0,0 +1,13 @@
+package fr.manastria.programmetele;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
